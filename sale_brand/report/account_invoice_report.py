903e2f42460301b986422328da2e14daeb136ad3
gflores <gustavofc65@gmail.com>
2021-09-30 17:06:15 -0500

    [product_brand] account invoice report4

--- sale_brand/report/account_invoice_report.py
+++ sale_brand/report/account_invoice_report.py
@@ -26,41 +26,36 @@ _logger = logging.getLogger(__name__)
 
 
 class AccountInvoiceReport(models.Model):
-    # _name = "account.invoice.report"
+    _name = "account.invoice.report"
     _inherit = 'account.invoice.report'
 
-    product_brand_id = fields.Many2one(
-        comodel_name='product.brand',
-        string='Brand',
-    )
-
-    # def _from(self):
-    #     from_str = super(AccountInvoiceReport, self)._from()
-    #     from_str += """
-    #     LEFT JOIN product_brand pb ON pb.id = template.product_brand_id
-    #     """
-    #     return from_str
-
-    def _select(self):
-        select_str = super(AccountInvoiceReport, self)._select()
-        select_str += """
-        , pb.name as brand
+    def _from(self):
+        from_str = super(AccountInvoiceReport, self)._from()
+        from_str += """
+        LEFT JOIN product_brand pb ON pb.id = template.product_brand_id
         """
-        # ,
-        return select_str
+        return from_str
 
-    def _sub_select(self):
-        sub_select_str = super(AccountInvoiceReport, self)._sub_select()
-        sub_select_str += """
-        , pb.name
-        """
-        # , pb.name
-        return sub_select_str
-
+    @api.model
     def _group_by(self):
         group_by_str = super(AccountInvoiceReport, self)._group_by()
         group_by_str += """
         , pb.name
         """
-        # , pb.name
         return group_by_str
+
+    @api.model
+    def _sub_select(self):
+        sub_select_str = super(AccountInvoiceReport, self)._sub_select()
+        sub_select_str += """
+        , pb.name as brand
+        """
+        return sub_select_str
+
+    @api.model
+    def _select(self):
+        select_str = super(AccountInvoiceReport, self)._select()
+        select_str += """
+        , pb.name
+        """
+        return select_str
