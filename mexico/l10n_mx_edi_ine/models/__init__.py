41e6c1b85b58a222936d970260ee9e2d882cb630
gflores <gustavofc65@gmail.com>
2021-09-30 21:25:48 -0500

    modules v13

--- mexico/l10n_mx_edi_ine/models/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import account_invoice
-from . import l10n_mx_edi_ine_entity
--- mexico/l10n_mx_edi_pos/models/__init__.py
+++
@@ -1,5 +0,0 @@
-# Part of Odoo. See LICENSE file for full copyright and licensing details.
-
-from . import account
-from . import account_invoice
-from . import point_of_sale
